c692703474b37fc92f55094af583d4a33dd1db51
wea9677 <wea9677@naver.com>
2022-05-28 11:36:18 +0900

    코드 변경, 기록된 순서대로 내림차순 정리, 게시물 비밀번호가 틀리면 삭제, 수정 불가

--- routes/article.js
+++ routes/article.js
@@ -11,13 +11,16 @@ router.get("/", (req, res) => {
 
 
 router.get("/article", async (req, res) => {
-    const article = await Article.find();
+    const article = await Article.find().sort({createdAt:'desc'});
 
     res.json({
         article,
     });
     // res.sendFile(__dirname + "/article.html")
 });
+
+
+
 
 // 상세페이지
 
@@ -33,31 +36,42 @@ router.get("/article/:articleId", async (req, res) => {
 
 // 삭제하기
 router.delete("/article/:articleId", async (req, res) => {
-    const {articleId} = req.params;
+    const {articlePw ,articleId} = req.params;
     
     const removearticle = await Article.find({articleId : Number(articleId)}); //비밀번호를 비교하는 법을 찾지 못했습니다. ㅠㅠ
                                                                                //그리고 Thunder client 에서 비밀번호 값을 어떻게 비교 해서 검사를 하는지 방법도 잘 모르겠어요 ㅠㅠ
-    if (removearticle.length > 0) {
-      await Article.deleteOne({articleId: Number(articleId)});
+    if (removearticle.length ) {
+      if (removearticle[0].articlePw !== articlePw){ //글 지우기 전 입력받은 비밀번호 체크하기
+        res.status(400).json({'result':'success', 'msg':'비밀번호가 일치하지 않습니다.'})
+      } else {
+        await Article.deleteOne({articleId: Number(articleId)});
+        res.status(200).json({'result':'success', 'msg':'글이 삭제되었습니다.'})
+      }
     }
-    res.json({success: true});
+    //res.json({success: true});
   });
+
+
+
   
 // 수정하기  
   router.put("/article/:articleId", async (req, res) => {
     const {articleId} = req.params;
-    const {content} = req.body;
+    const {content, name, title, articlePw} = req.body;
 
     const updatearticle = await Article.find({articleId : Number(articleId)});
-    if (!updatearticle.length){
-        return res.status(400).json({success: false, errorMessage: "게시글이 없습니다."})
+    if (updatearticle.articlePw !== articlePw){
+      res.status(400).json({'result': 'error', 'msg': '비밀번호가 일치하지 않아요..!'})
 
+    }else{
+      const updatearticle = await Article.updateOne({articleId: Number(articleId)}, {$set: {content, name, title, articlePw}});
+      res.status(201).json({'result': 'success', 'msg': '글이 수정되었습니다.'})
     }
-    await Article.updateOne({articleId: Number(articleId)}, {$set: {content}});
+    
+    
 
-    res.json({success: true});
+    //res.json({success: true});
   });
-
 
 
 
